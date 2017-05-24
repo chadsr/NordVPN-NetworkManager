bb1e155c63b7a05904df0757b597b9efa9af5488
Ross Chadwick <ross@rchadwick.co.uk>
2017-05-24 12:30:53 +0200

    Cleanup and checks server/port is open before adding

--- import.py
+++ import.py
@@ -2,23 +2,17 @@
 import configparser
 import argparse
 
-from urllib import error, parse, request
-from os.path import basename
-import os, datetime, time
-import sys
+from urllib import error, request
+import os
 import subprocess
 from io import BytesIO
 from zipfile import ZipFile
-import requests
-import keyring
-from time import sleep
 
 import logging
 
 import pickle
 
-# Temp
-import json
+import socket
 
 TIMEOUT = 30
 
@@ -38,9 +32,9 @@ sudo nmcli connection import type openvpn file <FILE>
 TODO: Check if file has been modified and if so, replace old one with it
 """
 
+
 class ConfigHandler(object):
     def __init__(self):
-        #__attr__?
         self.data = configparser.ConfigParser()
 
         # Generate default config
@@ -71,6 +65,7 @@ class ConfigHandler(object):
     def get_whitelist(self):
         return self.data['General']['country-whitelist'].split(' ')
 
+
 class Importer(object):
     def __init__(self):
         logging.basicConfig(filename='error.log', level=logging.ERROR)
@@ -90,32 +85,51 @@ class Importer(object):
         if os.path.isfile(LIST_PATH):
             self.active_list = self.load_active_list(LIST_PATH)
 
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument("--sync", help="Synchronise any new connections, whilst preserving existing connections. (default)", action="store_true")
+        self.parser.add_argument("--purge", help="Remove all active connections.", action="store_true")
+        self.parser.add_argument("--clean-sync", help="Remove all active connections and synchronise.", action="store_true")
+
     def start(self):
-        self.download_configs("https://nordvpn.com/api/files/zip")
-        self.sync_imports()
+        args = self.parser.parse_args()
+
+        if args.sync:
+            self.sync_imports()
+        elif args.purge:
+            self.purge_active_connections()
+        elif args.clean_sync:
+            self.purge_active_connections()
+            self.sync_imports()
+        else:
+            self.sync_imports()
+
         self.save_active_list(self.active_list, LIST_PATH)
-        subprocess.run("sudo service network-manager restart", shell=True, check=True)
+        print("Restarting NetworkManager.")
+        subprocess.run("systemctl restart NetworkManager.service", shell=True, check=True)
 
     def add_credentials(self, filename):
-        config = configparser.ConfigParser()
-        path = "/etc/NetworkManager/system-connections/"+filename
+        try:
+            config = configparser.ConfigParser()
+            path = "/etc/NetworkManager/system-connections/"+filename
 
-        if os.path.isfile(path):
-            config.read(path)
-        elif os.path.isfile(path+'_'):
-            path = path+'_'
-            config.read(path)
-        else:
-            print ("No config file found!", path)
-            return
+            if os.path.isfile(path):
+                config.read(path)
+            elif os.path.isfile(path+'_'):
+                path = path+'_'
+                config.read(path)
+            else:
+                print("VPN file not found!", path)
+                return
 
-        config['vpn']['password-flags'] = "0"
-        config['vpn']['username'] = self.config.get_username()
-        config['vpn-secrets'] = {}
-        config['vpn-secrets']['password'] = self.config.get_password()
+            config['vpn']['password-flags'] = "0"
+            config['vpn']['username'] = self.config.get_username()
+            config['vpn-secrets'] = {}
+            config['vpn-secrets']['password'] = self.config.get_password()
 
-        with open(path, 'w') as config_file:
-            config.write(config_file)
+            with open(path, 'w') as config_file:
+                config.write(config_file)
+        except Exception as e:
+            print(e)
 
     def import_ovpn(self, filename):
         subprocess.run("nmcli connection import type openvpn file "+DATA_DIR+filename, shell=True, check=True)
@@ -125,11 +139,10 @@ class Importer(object):
 
     def remove_connection(self, connection_name):
         subprocess.run("nmcli connection delete "+connection_name, shell=True, check=True)
-        print (connection_name)
-        self.active_list.remove(connection_name)
 
     def download_configs(self, url):
         try:
+            print("Downloading latest configs from", url)
             uo = request.urlopen(url, timeout=TIMEOUT)
             zipfile = ZipFile(BytesIO(uo.read()))
 
@@ -137,24 +150,69 @@ class Importer(object):
         except (error.URLError) as e:
             logging.error(e)
 
+    def purge_active_connections(self):
+        for connection in self.active_list:
+            self.remove_connection(connection)
+
+        self.active_list = []
+
     def load_active_list(self, path):
-        with open (LIST_PATH, 'rb') as fp:
+        with open(LIST_PATH, 'rb') as fp:
             itemlist = pickle.load(fp)
         return itemlist
 
     def save_active_list(self, itemlist, path):
         with open(LIST_PATH, 'wb') as fp:
             pickle.dump(itemlist, fp)
+        print("Active list saved.")
 
     def get_country_code(self, connection_name):
         # hacky
         return connection_name[:2]
 
+    def test_connection(self, filename):
+        protocol = ""
+        host = ""
+        port = ""
+
+        with open(DATA_DIR+filename) as config:
+            for line in config:
+                if not line.isspace():
+                    split = line.split()
+                    name = split[0]
+                    if name == "proto":
+                        protocol = split[1]
+                    elif name == "remote":
+                        host = split[1]
+                        port = int(split[2])
+
+        if protocol == "tcp":
+            socket_type = socket.SOCK_STREAM
+        elif protocol == "udp":
+            socket_type = socket.SOCK_DGRAM
+        else:
+            return False
+
+        s = socket.socket(socket.AF_INET, socket_type)
+        try:
+            s.settimeout(5)
+            s.connect((host, port))
+            s.shutdown(2)
+            return True
+        except Exception as e:
+            return False
+
     def sync_imports(self):
+        self.download_configs("https://nordvpn.com/api/files/zip")
+
         # first check if anything needs purging from the existing connections
+        purged = []
         for connection in self.active_list:
             if self.get_country_code(connection) in self.black_list:
                 self.remove_connection(connection)
+                purged.append(connection)
+
+        self.active_list.remove(purged)
 
         # Then begin checking for new
         for filename in os.listdir(DATA_DIR):
@@ -163,10 +221,13 @@ class Importer(object):
 
             if self.white_list:
                 if (split_name[0] not in self.active_list) and (country_code in self.white_list):
-                    self.import_ovpn(filename)
+                    if (self.test_connection(filename)):
+                        self.import_ovpn(filename)
             else:
                 if (split_name[0] not in self.active_list) and (country_code not in self.black_list):
-                    self.import_ovpn(filename)
+                    if (self.test_connection(filename)):
+                        self.import_ovpn(filename)
+
 
 importer = Importer()
 importer.start()
