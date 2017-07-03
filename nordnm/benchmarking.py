7687bb8e2f6e557f38e1286f5f8fde9fa4862cb4
Ross Chadwick <ross@rchadwick.co.uk>
2017-07-03 14:55:13 +0200

    multiprocessing experiments

--- nordnm/benchmarking.py
+++ nordnm/benchmarking.py
@@ -5,6 +5,8 @@ import multiprocessing
 from functools import partial
 import resource
 import numpy
+import os
+import subprocess
 
 
 def generate_connection_name(server, protocol):
@@ -62,8 +64,16 @@ def compare_server(server, best_servers, ping_attempts):
 
 def get_num_processes(num_servers):
     # Since each process is not resource heavy and simply takes time waiting for pings, maximise the number of processes (within constraints of the current configuration)
+
+    # Maximum open file descriptors of current configuration
     soft_limit, _ = resource.getrlimit(resource.RLIMIT_NOFILE)
-    max_processes = int(soft_limit/multiprocessing.cpu_count())  # This doesn't particularly make sense right now...
+
+    # Find how many file descriptors are already in use by the parent process
+    ppid = os.getppid()
+    used_file_descriptors = int(subprocess.run('ls -l /proc/'+str(ppid)+'/fd | wc -l', shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8'))
+
+    # Max processes is the number of file descriptors left, before the sof limit (configuration maximum) is reached
+    max_processes = soft_limit - used_file_descriptors
 
     if num_servers > max_processes:
         return max_processes
@@ -77,6 +87,7 @@ def get_best_servers(server_list, ping_attempts):
 
     num_servers = len(server_list)
     num_processes = get_num_processes(num_servers)
+    print("Number of processes:", num_processes)
 
     pool = multiprocessing.Pool(num_processes)
     pool.map(partial(compare_server, best_servers=best_servers, ping_attempts=ping_attempts), server_list)
